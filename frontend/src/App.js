574f8c4c0f4b77b00805fefb68644a1625b00ddc
gershomlapaix <nsengiyumv33@gmail.com>
2022-02-17 17:34:34 +0200

    add designs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./index.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div class="header__logo-box">
+          <img src="https://cdn.dribbble.com/users/6225232/screenshots/15138375/media/90a48df2fc5ddef462542aafd1f9041c.jpg?compress=1&resize=400x300" alt="Logo" class="header__logo" />
+        </div>
+
+        <div class="header__text-box">
+          <h1 class="heading-primary">
+            <span class="heading-primary--main">Electricity</span>
+            <span class="heading-primary--sub">We provide you good services</span>
+          </h1>
+
+          <a href="#" class="btn btn--white btn--animated">
+            Get started
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
